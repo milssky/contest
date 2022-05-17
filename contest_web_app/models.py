9c6bddb5be68c1660cfa77a232ed56b0baefa830
Alexey Potapov <2furius@gmail.com>
2022-05-17 21:54:47 +0300

    Создана модель задачи

--- contest_web_app/models.py
+++ contest_web_app/models.py
@@ -1,3 +1,45 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Course(models.Model):
+    pass
+
+
+class Task(models.Model):
+    course = models.ForeignKey(
+        Course,
+        on_delete=models.SET_NULL,
+        related_name="tasks",
+        blank=False,
+        null=False,
+        verbose_name="Курс",
+        help_text="Выберите курс для привязки задачи"
+    )
+    title = models.CharField(
+        max_length=255,
+        verbose_name="Заголовок",
+        help_text="Введите название задачи"
+    )
+    task_text = models.TextField(blank=False)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    score = models.PositiveSmallIntegerField(
+        verbose_name="Оценка",
+        validators=[
+            MinValueValidator(1, "Баллов не может быть меньше 1"),
+            MaxValueValidator(10, "Баллов не может быть больше 10")
+        ]
+    )
+    language = models.CharField(max_length=50)  # @TODO добавить выбор языка задачи
+
+    def __str__(self):
+        return f"{self.course}-{self.title}"
+
+    class Meta:
+        ordering = ("-pub_date",)
+        verbose_name = "Задача"
+        verbose_name_plural = "Задачи"
+
+
+class Attempt(models.Model):
+    pass
